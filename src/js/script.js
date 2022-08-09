83aaeb5d9f1c7f03dc3022b843fd8320fe728efe
Alex <rengarsanj@gmail.com>
2022-08-08 17:31:25 -0800

    08.08.2022first

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+// tiny slider 
+var slider = tns({
+    container: '.slider-section__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  })
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  })
+
+
+//   var slider = tns({
+//     container: '.my-slider',
+//     items: 1,
+//     responsive: {
+//       640: {
+//         edgePadding: 20,
+//         gutter: 20,
+//         items: 2
+//       },
+//       700: {
+//         gutter: 30
+//       },
+//       900: {
+//         items: 3
+//       }
+//     }
+//   });
