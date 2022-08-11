3ee40a974a147ccd3c875369c01dcc4f1db7f08a
Alex <rengarsanj@gmail.com>
2022-08-10 22:00:25 -0800

    10.08.20222

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,24 @@ var slider = tns({
     nav: false
   });
 
+  
+   // this script to make element as button for slider 
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
-  })
+  });
 
+
+  // this script to make element as button for slider 
   document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
-  })
+  });
 
+  // skript for tabs 
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
 
 // tiny slider adaptive 
 //   var slider = tns({
