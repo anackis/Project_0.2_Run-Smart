d75f844cde4263daf77fed39a6f39ea364242d18
Alex <rengarsanj@gmail.com>
2022-08-17 23:56:24 -0800

    17.08.2022

--- src/js/script.js
+++ src/js/script.js
@@ -118,7 +118,26 @@ $(document).ready(function() {
 	
 
 
+	$('form').submit(function(e) {
+		e.preventDefault();
 
+		// if (!$(this).valid()) {
+		// 	return;
+		// }
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn();
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 
 
 
