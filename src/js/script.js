e90a067054cb1a70d2d664cdd11d91fadca23cf4
Alex <rengarsanj@gmail.com>
2022-08-23 20:57:24 -0800

    23.08.2022

--- src/js/script.js
+++ src/js/script.js
@@ -140,6 +140,67 @@ $(document).ready(function() {
 	});
 
 
+	// Script for hamburger 
+	window.addEventListener('DOMContentLoaded', () => {
+		const menu = document.querySelector('.menu'),
+		menuItem = document.querySelectorAll('.menu__item'),
+		hamburger = document.querySelector('.hamburger');
+	
+		hamburger.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('menu_active');
+		});
+	
+		menuItem.forEach(item => {
+			item.addEventListener('click', () => {
+				hamburger.classList.toggle('hamburger_active');
+				menu.classList.toggle('menu_active');
+			})
+		})
+	});
+
+
+
+	// Skript for page up icon fadein and fade out
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 700) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+
+	// smooth scroll alternative 
+	// $("a[href^='#']").click(function() {
+	// 	const _href = $(this).attr("href");
+	// 	$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+	// 	return false;
+	// });
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+		  // Prevent default anchor click behavior
+		  event.preventDefault();
+	
+		  // Store hash
+		  var hash = this.hash;
+	
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+	
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+		} // End if
+	  });
+
 
 
 });
